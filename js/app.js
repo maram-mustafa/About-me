2cff9f3c8363a62ec25d2c813fced6c9970187c3
maram mustafa <maram.w.mustafa@gmail.com>
2021-04-07 14:59:23 +0300

    edit

--- js/app.js
+++ js/app.js
@@ -1,146 +1,146 @@
 'use strict';
 
-let userName = prompt('please enter your name!');
-alert('welcome to my website ' + userName);
+// let userName = prompt('please enter your name!');
+// alert('welcome to my website ' + userName);
 
 
-let qusOne = prompt('Do you think my name is maram ?!');
-//console.log(qusOne);
-//alert(qusOne);
-qusOne = qusOne.toLowerCase();
+// let qusOne = prompt('Do you think my name is maram ?!');
+// //console.log(qusOne);
+// //alert(qusOne);
+// qusOne = qusOne.toLowerCase();
 
-switch(qusOne){
-    case 'yes':
-    case 'y':
-        alert('yes! my name is maram');
-        break;
+// switch(qusOne){
+//     case 'yes':
+//     case 'y':
+//         alert('yes! my name is maram');
+//         break;
 
-    case 'no':
-    case 'n':
-        alert('Wrong answar');
-        break;
+//     case 'no':
+//     case 'n':
+//         alert('Wrong answar');
+//         break;
 
-        default:
-        alert ('please input a valid value');
+//         default:
+//         alert ('please input a valid value');
         
-}
+// }
 
-let qusTwo = prompt('Do you think I studied programming ?');
-//console.log(qusTwo);
-//alert(qusTwo);
-qusTwo = qusTwo.toLowerCase();
+// let qusTwo = prompt('Do you think I studied programming ?');
+// //console.log(qusTwo);
+// //alert(qusTwo);
+// qusTwo = qusTwo.toLowerCase();
 
-switch(qusTwo){
-    case 'yes':
-    case 'y':
-        alert('yes! programming is my fild of study');
-         break;
+// switch(qusTwo){
+//     case 'yes':
+//     case 'y':
+//         alert('yes! programming is my fild of study');
+//          break;
 
-    case 'no':
-    case'n':
-        alert('wrong answar');
-        break;
+//     case 'no':
+//     case'n':
+//         alert('wrong answar');
+//         break;
 
-    default:
-        alert('please input a valid value');
+//     default:
+//         alert('please input a valid value');
 
-}
-
-
-
-let qusThree = prompt('Is the University of Jordan the one that I studied at or not?');
-//console.log(qusThree);
-//alert(qusThree);
-qusThree = qusThree.toLowerCase();
-
-switch(qusThree){
-    case 'yes':
-    case 'y':
-        alert('yes! im studied in the university of jordan');
-        break;
-
-    case 'no':
-    case 'n':
-        alert('Wrong answar');
-        break;
-
-        default:
-            alert('please input a valid value');
-            break;
-}
-
-let qusFour = prompt('Do you think I got certificates in another field of IT?');
-//console.log(qusFour);
-//alert(qusFour);
-qusFour = qusFour.toLowerCase();
-
-switch(qusFour){
-    case 'yes':
-    case 'y':
-        alert('yes, i took a certificates in ccna');
-        break;
-
-    case 'no':
-    case 'n':
-        alert('Wrong answar');
-        break;
-
-        default:
-            alert('please input a valid value');
-            break;
-}
+// }
 
 
 
-let qusFive = prompt('Do you think the first field I got deeper into was programming?');
-//console.log(qusFive);
-//alert(qusFive);
-qusFive = qusFive.toLowerCase();
+// let qusThree = prompt('Is the University of Jordan the one that I studied at or not?');
+// //console.log(qusThree);
+// //alert(qusThree);
+// qusThree = qusThree.toLowerCase();
 
-switch(qusFive){
-    case 'yes':
-    case 'y':
-        alert('Wrong answar');
-        break;
+// switch(qusThree){
+//     case 'yes':
+//     case 'y':
+//         alert('yes! im studied in the university of jordan');
+//         break;
 
-    case 'no':
-    case 'n':
-        alert('Unfortunately yes, it was not the first field');
-        break;
+//     case 'no':
+//     case 'n':
+//         alert('Wrong answar');
+//         break;
 
-        default:
-            alert('please input a valid value');
-            break;
-}
+//         default:
+//             alert('please input a valid value');
+//             break;
+// }
 
-alert('Thanks for visiting my site ' + userName);
+// let qusFour = prompt('Do you think I got certificates in another field of IT?');
+// //console.log(qusFour);
+// //alert(qusFour);
+// qusFour = qusFour.toLowerCase();
+
+// switch(qusFour){
+//     case 'yes':
+//     case 'y':
+//         alert('yes, i took a certificates in ccna');
+//         break;
+
+//     case 'no':
+//     case 'n':
+//         alert('Wrong answar');
+//         break;
+
+//         default:
+//             alert('please input a valid value');
+//             break;
+// }
 
 
+
+// let qusFive = prompt('Do you think the first field I got deeper into was programming?');
+// //console.log(qusFive);
+// //alert(qusFive);
+// qusFive = qusFive.toLowerCase();
+
+// switch(qusFive){
+//     case 'yes':
+//     case 'y':
+//         alert('Wrong answar');
+//         break;
+
+//     case 'no':
+//     case 'n':
+//         alert('Unfortunately yes, it was not the first field');
+//         break;
+
+//         default:
+//             alert('please input a valid value');
+//             break;
+// }
+
+// alert('Thanks for visiting my site ' + userName);
 
 
 
 
 
-for (let i = 0; i < 4; i++) {
-    let number = 4;
-    let guess = prompt('guess a number!');
 
-    if (parseInt(guess) === parseInt(number)) {
-        alert('YOU GOT IT RIGHT');
-        break;
 
-    } else if (parseInt(guess) > parseInt(number)) {
-        alert('TOO HIGH! GUESS AGAIN!');
+// for (let i = 0; i < 4; i++) {
+//     let number = 4;
+//     let guess = prompt('guess a number!');
 
-    } else if (parseInt(guess) < parseInt(number)) {
-        alert('TOO LOW! GUESS AGAIN');
+//     if (parseInt(guess) === parseInt(number)) {
+//         alert('YOU GOT IT RIGHT');
+//         break;
 
-    } else {
-        alert('ENTER A NUMBER');
+//     } else if (parseInt(guess) > parseInt(number)) {
+//         alert('TOO HIGH! GUESS AGAIN!');
 
-    }
-}
-alert('You exhausted your 4 attempts, correct answer # 4');
+//     } else if (parseInt(guess) < parseInt(number)) {
+//         alert('TOO LOW! GUESS AGAIN');
+
+//     } else {
+//         alert('ENTER A NUMBER');
+
+//     }
+// }
+// alert('You exhausted your 4 attempts, correct answer # 4');
 
 
  
@@ -153,7 +153,7 @@ let answer= null;
         for(let y= 0 ; y< talent.length; y++){
 
             if(answer === talent[y]){
-                alert('correct');
+                alert('correct '  );
                 score++;
                 alert('your score ' + score);
                 y=50;
@@ -162,10 +162,12 @@ let answer= null;
     
         if (x === 5){
             alert('you didnt answer correct ');
-            
+            alert('the corect answer is dancing, acting, singing, painting, reading '); 
+            alert('your score ' + score);
         }
 
     }
+    
 
 
 
